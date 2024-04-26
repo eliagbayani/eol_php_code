d4810a035e1efc49d48649566478b217a2b0ac96
eliagbayani <eagbayani@eol.org>
2024-04-27 00:27:54 +0800

    Update 723.php

--- update_resources/connectors/723.php
+++ update_resources/connectors/723.php
@@ -16,7 +16,7 @@ $timestart = time_elapsed();
 $resource_id = 723;
 $func = new NCBIGGIqueryAPI($resource_id);
 
-$funct->start();
+$func->start();
 Functions::finalize_dwca_resource($resource_id, false, true); //false here means not a big file, true means delete working folder.
 
 /* not yet implemented
