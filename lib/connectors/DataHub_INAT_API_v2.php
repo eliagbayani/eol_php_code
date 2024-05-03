21758928bbd7eb81960f4ed327171a39c1f3efc3
eliagbayani <eagbayani@eol.org>
2024-05-03 15:36:48 +0800

    Update DataHub_INAT_API_v2.php

--- lib/connectors/DataHub_INAT_API_v2.php
+++ lib/connectors/DataHub_INAT_API_v2.php
@@ -14,30 +14,43 @@ class DataHub_INAT_API_v2
             $this->archive_builder = new \eol_schema\ContentArchiveBuilder(array('directory_path' => $this->path_to_archive_directory));    
         }
 
-        $this->inat_api['taxa'] = "https://api.inaturalist.org/v1/observations/species_counts?taxon_is_active=true&hrank=species&lrank=species&iconic_taxa=XGROUP&quality_grade=research&page=XPAGE"; //defaults to per_page = 500
+        $this->inat_api['taxa'] = "https://api.inaturalist.org/v1/observations/species_counts?taxon_is_active=true&hrank=species&lrank=species&iconic_taxa=XGROUP&quality_grade=XGRADE&page=XPAGE"; //defaults to per_page = 500
         if(Functions::is_production()) $save_path = "/extra/other_files/dumps_GGI/";
         else                           $save_path = "/Volumes/Crucial_2TB/other_files2/dumps_GGI/";
         if(!is_dir($save_path)) mkdir($save_path);
         $save_path = $save_path . "iNat/";
         if(!is_dir($save_path)) mkdir($save_path);
 
-        $this->dump_file = $save_path . "/datahub_inat_v2.txt";
-        if(is_file($this->dump_file)) unlink($this->dump_file);
+        $this->dump_file['research'] = $save_path . "/datahub_inat_grade_research.txt";
+        if(is_file($this->dump_file['research'])) unlink($this->dump_file['research']);
+
+        $this->dump_file['needs_id'] = $save_path . "/datahub_inat_grade_needs_id.txt";
+        if(is_file($this->dump_file['needs_id'])) unlink($this->dump_file['needs_id']);
 
         $this->reports_path = $save_path;
         $this->taxon_page = "https://www.inaturalist.org/taxa/"; //1240-Dendragapus or just 1240
     }
     function start()
     {
+        $quality_grades = array('research', 'needs_id');
         $groups = array("Insecta", "Plantae", "Actinopterygii", "Amphibia", "Arachnida", "Aves", "Chromista", "Fungi", "Mammalia", "Mollusca", "Reptilia", "Protozoa", "unknown");
-        $groups = array("Insecta");
-        foreach($groups as $group) {
-            self::get_iNat_taxa_using_API($group);
+        // $groups = array("Insecta");
+        // $groups = array("Fungi", "Mammalia", "Mollusca", "Reptilia", "Protozoa", "unknown");
+
+        foreach($quality_grades as $grade) {
+            foreach($groups as $group) {
+                echo "\nProcessing [$group]...[$grade]...\n";
+                self::get_iNat_taxa_using_API($group, $grade);
+                echo "\nEvery group, sleep 1 min.\n";
+                // sleep(60*1); //10 mins interval per group
+            }    
         }
     }
-    function get_iNat_taxa_using_API($group) //not advisable to use, bec. of the 10,000 limit page coverage
+    function get_iNat_taxa_using_API($group, $grade) //not advisable to use, bec. of the 10,000 limit page coverage
     {
         $main_url = str_replace("XGROUP", $group, $this->inat_api['taxa']);
+        $main_url = str_replace("XGRADE", $grade, $main_url);
+
         $page = 1;
         $url = str_replace("XPAGE", $page, $main_url);
 
@@ -47,6 +60,12 @@ class DataHub_INAT_API_v2
         $pages = ceil($total / 500);  echo "\ntotal_results: [$total]\ntotal pages: [$pages]\n";
 
         for($page = 1; $page <= $pages; $page++) {
+
+            if(($page % 50) == 0) {
+                echo "\nEvery 50 calls, sleep 10 mins.\n";
+                // sleep(60*5); //10 mins interval
+            }
+
             $url = str_replace("XPAGE", $page, $main_url);
             if($json = Functions::lookup_with_cache($url, $this->download_options_INAT)) {
                 $obj = json_decode($json); //print_r($obj); exit;
@@ -59,7 +78,7 @@ class DataHub_INAT_API_v2
                     $rek["sciname"]             = $t->name;
                     $rek["parent_id"]           = $t->parent_id;
                     $rek["observations_count"]   = $t->observations_count;
-                    self::save_to_dump($rek, $this->dump_file);    
+                    self::save_to_dump($rek, $this->dump_file[$grade]);    
                 }
             }
             // if($page >= 6) break; //dev only
