54c0b4fc096f57e4a180cd690a6ace789d24633b
eliagbayani <eagbayani@eol.org>
2024-06-16 21:17:08 +0800

    Update DataHub_NCBI_API.php

--- lib/connectors/DataHub_NCBI_API.php
+++ lib/connectors/DataHub_NCBI_API.php
@@ -44,7 +44,7 @@ class DataHub_NCBI_API
         $this->dumpfile["prot.accession2taxid"] = "https://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/prot.accession2taxid.FULL.INDEX-NUM.gz";
         $this->dumpfile["prot.accession2taxid"] = "http://localhost/other_files2/dumps_GGI/NCBI/prot/prot.accession2taxid.FULL.INDEX-NUM.gz"; //dev only
 
-        /* will add this to jenkins
+        /* will add this to jenkins. We need this if we want totals from dump files.
         wget -c https://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/prot.accession2taxid.FULL.1.gz
         wget -c https://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/prot.accession2taxid.FULL.2.gz
         wget -c https://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/prot.accession2taxid.FULL.3.gz
@@ -88,7 +88,7 @@ class DataHub_NCBI_API
                 // exit("\nelix 3\n");
         --- end step 1 --- */
 
-        /* step 2: process genus and family; loop tsv file and use API        
+        /* step 2: process genus and family; loop tsv file and use API
         self::parse_tsv_file($this->dump_file, "process genus family from compiled taxonomy");
         exit("\n-caching ends-\n"); //comment in real operation
         --- end step 2 --- */
