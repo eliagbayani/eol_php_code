b7c37b40c0dee95c58170c0b2892d65e926430c3
eliagbayani <eagbayani@eol.org>
2024-06-16 22:19:09 +0800

    Update datahub_ncbi.php

--- update_resources/connectors/datahub_ncbi.php
+++ update_resources/connectors/datahub_ncbi.php
@@ -11,13 +11,23 @@ $resource_id = 'NCBI';
 $func = new DataHub_NCBI_API($resource_id);
 
 // /*
-$func->start(); 
+$func->start();
 // */
 
 // /*
 // $func->parse_tsv_then_generate_dwca();
-Functions::finalize_dwca_resource($resource_id, false, true); //false here means not a big file, true means delete working folder.
+Functions::finalize_dwca_resource($resource_id, false, false); //false here means not a big file, true means delete working folder.
 // */
+
+// /* utility ========================== start
+require_library('connectors/DWCADiagnoseAPI');
+$func = new DWCADiagnoseAPI();
+
+$undefined_parents = $func->check_if_all_parents_have_entries($resource_id, false); //true means output will write to text file
+echo "\nTotal undefined parents:" . count($undefined_parents)."\n"; unset($undefined_parents);
+
+recursive_rmdir(CONTENT_RESOURCE_LOCAL_PATH."$resource_id/"); //we can now delete folder after DWCADiagnoseAPI()
+// /* utility ========================== end
 
 $elapsed_time_sec = time_elapsed() - $timestart;
 echo "\n\n";
