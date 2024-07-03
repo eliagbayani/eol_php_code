9aca024f8e90887da63520b3fa0623b39b160120
eliagbayani <eagbayani@eol.org>
2024-07-03 17:39:31 +0800

    Update form_result_map.php

--- applications/CheckListBank_tool/form_result_map.php
+++ applications/CheckListBank_tool/form_result_map.php
@@ -44,9 +44,14 @@ echo "<pre>"; print_r($form); echo "</pre>"; //exit("\neli 200\n");
 )*/
 $resource_id = @get_val_var('resource_id');
 $temp_dir = @get_val_var('temp_dir');
-debug("\n[$resource_id][$temp_dir]\n");
-$tables = array('taxonRank', 'taxonomicStatus', 'locality', 'occurrenceStatus');
+// debug("\n[$resource_id][$temp_dir]\n");
 
+$taxonRank_map          = generate_array_map($form, 'taxonRank'); //print_r($taxonRank_map); exit;
+$taxonomicStatus_map    = generate_array_map($form, 'taxonomicStatus');
+$locality_map           = generate_array_map($form, 'locality');
+$occurrenceStatus_map   = generate_array_map($form, 'occurrenceStatus');
+
+parse_TSV_file($temp_dir . 'Main_Table.txt', "");
 
 function get_val_var($v)
 {
@@ -61,5 +66,50 @@ function get_text_contents($basename)
     $contents = file_get_contents($filename);
     return explode("\n", $contents);
 }
+function generate_array_map($form, $table)
+{
+    $final = array();
+    foreach($form[$table] as $whole) {
+        $parts = explode("|", $whole);
+        $final[$parts[0]] = $parts[1];
+    }
+    return $final;
+}
+function parse_TSV_file($txtfile, $task)
+{   
+    $i = 0; debug("\nUpdating: [$txtfile]\n");
+    foreach(new FileIterator($txtfile) as $line_number => $line) {
+        if(!$line) continue;
+        $i++; if(($i % 1000) == 0) echo "\n".number_format($i)." ";
+        $row = explode("\t", $line); // print_r($row);
+        if($i == 1) {
+            $fields = $row;
+            $fields = array_filter($fields); //print_r($fields);
+            continue;
+        }
+        else {
+            $k = 0; $rec = array();
+            foreach($fields as $fld) { $rec[$fld] = @$row[$k]; $k++; }
+        }
+        $rec = array_map('trim', $rec); //echo "<pre>";print_r($rec); echo "</pre>"; exit("\nstopx\n");
+        /*Array(
+            [scientific_nameID] => x4
+            [parent_nameID] => 
+            [accepted_nameID] => 
+            [pre_name_usage] => accepted
+            [name_usage] => 
+            [unacceptability_reason] => 
+            [rank_name] => Kingdom
+            [taxon_author] => 
+            [unit_name1] => 
+            [unit_name2] => 
+            [unit_name3] => 
+            [unit_name4] => 
+            [geographic_value] => 
+            [origin] => 
+            [referenceID] => d41d8cd98f00b204e9800998ecf8427e
+        )*/
 
+    }
+}
 ?>
