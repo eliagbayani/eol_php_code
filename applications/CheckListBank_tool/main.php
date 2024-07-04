265863ac7171dc294838a635a269538e34446279
eliagbayani <eagbayani@eol.org>
2024-07-05 01:06:04 +0800

    Update main.php

--- applications/CheckListBank_tool/main.php
+++ applications/CheckListBank_tool/main.php
@@ -36,7 +36,7 @@ else                      $browser_comment = "Choose File"; //Safari Chrome etc
         <td>
             <input type='text' name='Filename_ID' hidden>
             <input type='text' name='Short_Desc' hidden>
-            <input type="submit" value="Convert archive file">
+            <input type="submit" value="Convert archive file to ITIS format">
             <input type="reset" value="Reset">
         </td>
     </tr>
