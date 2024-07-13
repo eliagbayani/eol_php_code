03001dbeedf7f71c9ef7cdc7cdf67b38e0b5446e
eliagbayani <eagbayani@eol.org>
2024-07-14 00:54:55 +0800

    Update CheckListBankWebReference.php

--- lib/connectors/CheckListBankWebReference.php
+++ lib/connectors/CheckListBankWebReference.php
@@ -12,28 +12,27 @@ class CheckListBankWebReference
             [temp_resource_dir] => /opt/homebrew/var/www/eol_php_code/applications/content_server/resources_3/CheckListBank_files/1720883775/
             [temp_tool_folder] => /opt/homebrew/var/www/eol_php_code//applications/CheckListBank_tool/temp/
         )*/
+        $this->resource_id = $params['resource_id'];
+        $this->temp_dir = $params['temp_dir'];
+        $this->temp_folder = $params['temp_folder'];
+        self::create_web_form_reference();
+
+        // -listed_pub_date
+        // -isbn
+        // -issn
+        // -pub_comment                
     }
     function create_web_form_reference()
     {
         // echo "\ndito na siya...\n";
-        $taxonRanks = self::get_text_contents('taxonRank');
-        $taxonomicStatuses = self::get_text_contents('taxonomicStatus');
-        if(in_array('accepted', $taxonomicStatuses) || 
-           in_array('unaccepted', $taxonomicStatuses) ||
-           in_array('not accepted', $taxonomicStatuses)) {
-            $taxonomicStatus_map = $this->map['taxonomicStatus']['accepted_not_accepted'];
-            $accepted_or_valid = 'accepted';
-        }
-        elseif(in_array('valid', $taxonomicStatuses) || 
-               in_array('invalid', $taxonomicStatuses)) {
-            $taxonomicStatus_map = $this->map['taxonomicStatus']['valid_invalid'];
-            $accepted_or_valid = 'valid';
-        }
-        else echo "\nWarning: No mapping for taxonomicStatus.\n";
-        
-        $localities = self::get_text_contents('locality');
-        $occurrenceStatuses = self::get_text_contents('occurrenceStatus');
+        $references = self::get_text_contents('References');
+        $fields = explode("\t", $references[0]);
+        array_shift($references);
+        $included_fields = array("reference_author", "title", "publication_name", "actual_pub_date", "listed_pub_date", "publisher", "pub_place", "pages", "isbn", "issn", "pub_comment");
+        // included_fields: the 11 Jen fields
 
+        echo "<pre>"; 
+        print_r($fields); //echo"<hr>"; print_r($references);
         ?>
         <table border="1" cellpadding="15" cellspacing="1" align="center" width="40%">
         <tr align="center"><td><b>CheckListBank Tool</b></td></tr>
@@ -44,109 +43,56 @@ class CheckListBankWebReference
                 <input type='hidden' name='resource_id' value='$this->resource_id'>
                 <input type='hidden' name='temp_dir' value='$this->temp_dir'>
                 <input type='hidden' name='temp_folder' value='$this->temp_folder'>
-                <input type='hidden' name='accepted_or_valid' value='$accepted_or_valid'>
                 "; 
-
                 ?>
         </td></tr>
         <tr align="center">
-            <td>
-                <table><tr rowspan='2'><td align='center' colspan='2'><b>rank_name</b><br>&nbsp;</td></tr><?php
-                $i = -1;
-                foreach($taxonRanks as $r) { $i++;
-                        ?><tr><?php
-                        if(!$r) continue;
-                        echo "<td>$r</td>";
-                        echo "<td>
-                            <select name='taxonRank[$i]' id='tR'>";
-                            foreach($this->map['taxonRank'] as $tr) {
-                                if($r == $tr) $selected = "selected";
-                                else          $selected = "";
-                                echo "<option value='$r|$tr' $selected >$tr</option>";
-                            }
-                            echo'</select>
-                        </td>';                            
-                        ?></tr><?php
-                } //end foreach()
-                ?></table>
-            </td>
-        </tr>
+            <td><!--- Array(
+                    [0] => Item type
+                    [1] => ID
+                    [2] => dwc
+                    [3] => reference_author
+                    [4] => Editors
+                    [5] => title
+                    [6] => publication_name
+                    [7] => actual_pub_date
+                    [8] => pages
+                    [9] => publisher
+                    [10] => pub_place
+                    [11] => URLs
+                    [12] => DOI
+                    [13] => Language
+                ) --->
+                <table><tr rowspan='2'><td align='center' colspan='2'><b>References</b><br>&nbsp;</td></tr><?php
+                $rows = 0;
+                foreach($references as $r) { $rows++; echo "<tr>";
+                    $ref = explode("\t", $r); //exit;
+                    
+                    $i = -1; foreach($fields as $fld) { $i++; $rek[$fld] = $ref[$i]; } //assignment
 
-        <tr align="center">
-            <td>
-                <table><tr rowspan='2'><td align='center' colspan='2'><b>name_usage | unacceptability_reason</b><br>&nbsp;</td></tr><?php
-                $i = -1;
-                foreach($taxonomicStatuses as $r) { $i++;
-                        ?><tr><?php
-                        if(!$r) continue;
-                        echo "<td>$r</td>";
-                        echo "<td>
-                            <select required name='taxonomicStatus[$i]' id='tS'>";?>
-                            <option value=""></option><?php
-                            if(in_array($r, array('accepted', 'not accepted', 'unaccepted', 'valid', 'invalid'))) {
-                                foreach($taxonomicStatus_map as $tr) {
-                                    if($r == $tr) $selected = "selected";
-                                    else          $selected = "";
-                                    echo "<option value='$r|$tr' $selected >$tr</option>";
-                                }    
-                            }
-                            else {
-                                foreach($this->map['unacceptability_reason'] as $tr) {
-                                    if($r == $tr) $selected = "selected";
-                                    else          $selected = "";
-                                    echo "<option value='$r|$tr' $selected >$tr</option>";
-                                }    
-                            }
-                            echo'</select>
-                        </td>';                            
-                        ?></tr><?php
-                } //end foreach()
-                ?></table>
-            </td>
-        </tr>
+                    foreach($rek as $fld => $val) {
+                        echo "<td>$fld</td><td>$val</td>";
+                    }
+                    echo "</tr>";
+                    if($rows >= 5) break; //debug only
+                }
 
-        <tr align="center">
-            <td>
-                <table><tr rowspan='2'><td align='center' colspan='2'><b>geographic_value</b><br>&nbsp;</td></tr><?php
-                $i = -1;
-                foreach($localities as $r) { $i++;
-                        ?><tr><?php
-                        if(!$r) continue;
-                        echo "<td>$r</td>";
-                        echo "<td>
-                            <select name='locality[$i]' id='l'>";
-                            foreach($this->map['locality'] as $tr) {
-                                if($r == $tr) $selected = "selected";
-                                else          $selected = "";
-                                echo "<option value='$r|$tr' $selected >$tr</option>";
-                            }
-                            echo'</select>
-                        </td>';                            
-                        ?></tr><?php
-                } //end foreach()
-                ?></table>
-            </td>
-        </tr>
 
-        <tr align="center">
-            <td>
-                <table><tr rowspan='2'><td align='center' colspan='2'><b>origin</b><br>&nbsp;</td></tr><?php
-                $i = -1;
-                foreach($occurrenceStatuses as $r) { $i++;
-                        ?><tr><?php
-                        if(!$r) continue;
-                        echo "<td>$r</td>";
-                        echo "<td>
-                            <select name='occurrenceStatus[$i]' id='oS'>";
-                            foreach($this->map['occurrenceStatus'] as $tr) {
-                                if($r == $tr) $selected = "selected";
-                                else          $selected = "";
-                                echo "<option value='$r|$tr' $selected >$tr</option>";
-                            }
-                            echo'</select>
-                        </td>';                            
-                        ?></tr><?php
-                } //end foreach()
+                // foreach($references as $r) { $i++;
+                //         ?><tr><?php
+                //         if(!$r) continue;
+                //         echo "<td>$r</td>";
+                //         echo "<td>
+                //             <select name='taxonRank[$i]' id='tR'>";
+                //             foreach($this->map['taxonRank'] as $tr) {
+                //                 if($r == $tr) $selected = "selected";
+                //                 else          $selected = "";
+                //                 echo "<option value='$r|$tr' $selected >$tr</option>";
+                //             }
+                //             echo'</select>
+                //         </td>';                            
+                //         ?></tr><?php
+                // } //end foreach()
                 ?></table>
             </td>
         </tr>
@@ -158,7 +104,7 @@ class CheckListBankWebReference
         </td></tr>
         </form>
         </table>
-        <?php
+        <?php echo "</pre>";
     }
     private function get_text_contents($basename)
     {
