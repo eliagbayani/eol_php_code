433cf29582d66595cb204ca6b6161c5a2eaa572f
eliagbayani <eagbayani@eol.org>
2024-07-24 07:28:07 +0800

    Update main.php

--- applications/CheckListBank_tool/main.php
+++ applications/CheckListBank_tool/main.php
@@ -51,7 +51,40 @@ else                      $browser_comment = "Choose File"; //Safari Chrome etc
             <input type='text' name='Short_Desc' hidden>
             <input type="submit" value="Convert archive file to ITIS format">
             <input type="reset" value="Reset">
-            <!-- Under maintenance... -->
+        </td>
+    </tr>
+    </form>
+</table>
+<br>
+
+
+<table border="1" cellpadding="15" cellspacing="1" align="center" width="40%">
+    <tr align="center"><td><b>CheckListBank Tool</b>
+    <br><br><small>
+    Or if you already have your Taxa and References files, you can submit here for the final step.
+    </small>
+    </td></tr>
+    <form action="form_result.php" method="post" enctype="multipart/form-data">
+    <tr><td>
+            <font size="3">
+            <b>Taxa File</b> (plain text, tsv or csv): a taxa file in Darwin Core Archive format, but without a meta.xml file. 
+            In this case, the file should have headers that we can use to infer the mapping for each column. <br><br>
+            Upload user file: </font><input type="file" name="file_upload" id="file_upload" size="100">
+            <br><br><small>(.tab or .tsv or .txt or .csv) OR (.tab.zip, .tsv.zip, .txt.zip, .csv.zip)</small>
+    </td></tr>
+    <tr><td>
+            <font size="3">
+            <b>Taxa List</b> (plain text, txt): a simple, one column document, with one name per line. 
+            We will treat the content of this column as a scientificName value. <br><br>
+            Upload user file: </font><input type="file" name="file_upload3" id="file_upload3" size="100">
+            <br><br><small>(.txt) OR (.txt.zip)</small>
+    </td></tr>
+    <tr align="center">
+        <td>
+            <input type='text' name='Filename_ID' hidden>
+            <input type='text' name='Short_Desc' hidden>
+            <input type="submit" value="Insert Reference fields to Taxa file">
+            <input type="reset" value="Reset">
         </td>
     </tr>
     </form>
