fd72ae73b358e4145f81e74d7a1f877e96b87da3
eliagbayani <eagbayani@eol.org>
2024-07-31 11:23:18 +0800

    Update ZenodoAPI.php

--- lib/connectors/ZenodoAPI.php
+++ lib/connectors/ZenodoAPI.php
@@ -15,15 +15,71 @@ class ZenodoAPI
         $this->api['domain'] = 'https://zenodo.org';
         if(Functions::is_production()) $this->path_2_file_dat = '/extra/other_files/Zenodo/';
         else                           $this->path_2_file_dat = '/Volumes/OWC_Express/other_files/Zenodo/';
-        if(!is_dir($this->path_2_file_dat)) mkdir(path_2_file_dat);
+        if(!is_dir($this->path_2_file_dat)) mkdir($this->path_2_file_dat);
     }
 
     function start()
     {
-        echo "\n".ZENODO_TOKEN."\n";
+        // echo "\n".ZENODO_TOKEN."\n";
         // self::create_dataset();
-        $obj = self::retrieve_dataset('13136202'); self::upload_dataset($obj);
-        // self::publish_dataset();
+        // $obj = self::retrieve_dataset('13136202'); self::upload_dataset($obj); //worked OK
+        // $obj = self::retrieve_dataset('13136202'); self::publish_dataset($obj); //worked OK
+
+        // $obj = self::retrieve_dataset('13136202'); self::update_dataset($obj); //didn't work yet
+
+    }
+    private function update_dataset($obj)
+    {
+        /*
+        curl -i -H "Content-Type: application/json" -X PUT
+        --data '{"metadata": {"title": "My first upload", "upload_type": "poster", 
+                    "description": "This is my first upload", 
+                    "creators": [{"name": "Doe, John", "affiliation": "Zenodo"}]}}' https://zenodo.org/api/deposit/depositions/1234?access_token=ACCESS_TOKEN        
+        */
+        $input = array();
+        $input['metadata'] = array("communities" => array(array("identifier" => "eol")), //Example: [{'identifier':'eol'}]
+                            );        
+
+        $input['metadata'] = array( "title" => "Images list",
+        "upload_type" => "dataset", //controlled vocab.
+        "description" => "
+All the images in EOL, with minimal metadata:
+EOL content ID (can construct eol media page url, eg: https://eol.org/media/15263823)
+EOL page ID (can construct eol taxon page url, eg: https://eol.org/pages/45511218)
+Medium Source URL (if available, eg: https://animaldiversity.org/collections/contributors/phil_myers/ADW_birds_3_4_03/Passeriformes/Emberizidae/lark_sparrow7789/large.jpg)
+EOL Full-Size Copy URL (recommended for download, eg: https://content.eol.org/data/media/be/4a/08/30.d4f396d82dcceeb0615c194f99421c0c.jpg)
+License (eg: cc-by-nc-sa-3.0, see https://creativecommons.org/licenses/ for details)
+Copyright Owner (unless image is in the Public Domain)
+                            ",
+                            "creators" => array(array("name" => "EOL staff", "affiliation" => "Encyclopedia of Life")), 
+                            //Example: [{'name':'Doe, John', 'affiliation': 'Zenodo'}, 
+                            //          {'name':'Smith, Jane', 'affiliation': 'Zenodo', 'orcid': '0000-0002-1694-233X'}, 
+                            //          {'name': 'Kowalski, Jack', 'affiliation': 'Zenodo', 'gnd': '170118215'}]
+                            "keywords" => array("Aggregate Datasets"),
+                            // "publication_date" => "2020-02-04", //required. Date of publication in ISO8601 format (YYYY-MM-DD). Defaults to current date.                                                                        
+                            "notes" => "For questions or use cases calling for large, multi-use aggregate data files, please visit the EOL Services forum at http://discuss.eol.org/c/eol-services",
+                            "communities" => array(array("identifier" => "eol")), //Example: [{'identifier':'eol'}]
+                            "dates" => array(array("start" => "2020-02-04", "end" => "2020-02-04", "type" => "Created"), //CKAN's created
+                                             array("start" => "2024-07-26", "end" => "2024-07-26", "type" => "Updated"), //CKAN's updated   
+                                            ), //Example: [{"start": "2018-03-21", "end": "2018-03-25", "type": "Collected", "description": "Specimen A5 collection period."}]
+                            "related_identifiers" => array(array("relation" => "isSupplementTo", 
+                                                                 "identifier" => "https://eol.org/data/media_manifest.tgz",
+                                                                 "resource_type" => "dataset")), 
+                            //Example: [{'relation': 'isSupplementTo', 'identifier':'10.1234/foo'}, {'relation': 'cites', 'identifier':'https://doi.org/10.1234/bar', 'resource_type': 'image-diagram'}]
+                            "access_right" => "open", //defaults to 'open'
+                            "license" => "cc-by",
+                    );        
+
+        $json = json_encode($input); // echo "\n$json\n";
+        print_r($input);
+
+        $cmd = 'curl -i -H "Content-Type: application/json" -X PUT --data '."'$json'".' https://zenodo.org/api/deposit/depositions/'.$obj['id'].'?access_token='.ZENODO_TOKEN;
+        // $cmd = 'curl -i -H "Content-Type: application/json" -X PUT --data '."'$json'".' https://zenodo.org/api/deposit/depositions/13136202?access_token='.ZENODO_TOKEN;
+
+        // $cmd .= " 2>&1";
+        echo "\n$cmd\n";
+        $json = shell_exec($cmd);           echo "\n$json\n";
+        $obj = json_decode(trim($json));    print_r($obj);
     }
     private function retrieve_dataset($id)
     {
@@ -42,16 +98,15 @@ class ZenodoAPI
         $bucket = $obj['links']['bucket']; //e.g. https://zenodo.org/api/files/6c1d26b0-7b4a-41e3-a0e8-74cf75710946 // echo "\n[$bucket]\n";
         $cmd = 'curl --upload-file /path/to/your/file.dat https://zenodo.org/api/files/6c1d26b0-7b4a-41e3-a0e8-74cf75710946/file.dat?access_token='.ZENODO_TOKEN;
         $cmd = 'curl --upload-file '.self::generate_file_dat($obj).' '.$bucket.'/'.$obj['id'].'.dat?access_token='.ZENODO_TOKEN;
-
         echo "\n$cmd\n";
         $json = shell_exec($cmd);               echo "\n$json\n";
         $obj = json_decode(trim($json), true);  print_r($obj);
-
     }
-    private function publish_dataset()
+    private function publish_dataset($obj)
     {
-        
+        $publish = $obj['links']['publish']; //https://zenodo.org/api/deposit/depositions/13136202/actions/publish
         $cmd = 'curl -i -H "Content-Type: application/json" -X POST https://zenodo.org/api/deposit/depositions/13136202/actions/publish?access_token='.ZENODO_TOKEN;
+        $cmd = 'curl -i -H "Content-Type: application/json" -X POST '.$publish.'?access_token='.ZENODO_TOKEN;
         // $cmd .= " 2>&1";
         echo "\n$cmd\n";
         $json = shell_exec($cmd);           echo "\n$json\n";
