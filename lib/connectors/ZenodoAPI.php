795fbba98e8ec958039ca63b38b87b8ecb38fdb4
eliagbayani <eagbayani@eol.org>
2024-08-10 02:47:00 +0800

    Update ZenodoAPI.php

--- lib/connectors/ZenodoAPI.php
+++ lib/connectors/ZenodoAPI.php
@@ -172,7 +172,8 @@ class ZenodoAPI
                 // if(!in_array($title, array("Publications using EOL structured data: 2018"))) continue;
 
                 // if(!in_array($title, array("EOL v3 data model Ontologies: media_extension.xml"))) continue;
-                if(!in_array($title, array("DH2.1 working docs: DH1.1 working version"))) continue;
+                // if(!in_array($title, array("DH2.1 working docs: DH1.1 working version"))) continue;
+                if(!in_array($title, array("Test data sets: Amoebozoa Test"))) continue;
 
 
 
