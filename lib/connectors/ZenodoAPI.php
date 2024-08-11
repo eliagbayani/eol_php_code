bb5f897454990a4c0b4c3d252c8586170549d142
eliagbayani <eagbayani@eol.org>
2024-08-11 15:41:55 +0800

    Update ZenodoAPI.php

--- lib/connectors/ZenodoAPI.php
+++ lib/connectors/ZenodoAPI.php
@@ -160,7 +160,7 @@ class ZenodoAPI
                 // if($r['name'] != 'EOL Fossil Fishes Patch ') continue;
                 // if($r['name'] != 'vernacular names, May 2020') continue;
                 // if($r['name'] != 'User Added Text, curated') continue; //"User Generated Content (EOL v2): User Added Text, curated"
-                if($r['name'] != 'EOL Hierarchy Entries April 2017') continue;
+                if($r['name'] != 'Hierarchy Entries April 2017') continue;
 
 
                 $input = self::generate_input_field($p, $r, $resources); //main operation
