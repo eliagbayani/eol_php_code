0120f8d144b2dd25812bcc90e9597546b9a272a2
eliagbayani <eagbayani@eol.org>
2024-09-04 07:38:10 +0800

    Update ZenodoAPI.php

--- lib/connectors/ZenodoAPI.php
+++ lib/connectors/ZenodoAPI.php
@@ -776,8 +776,7 @@ class ZenodoAPI extends ZenodoConnectorAPI
     {
         if($publish = @$obj['links']['publish']) { //https://zenodo.org/api/deposit/depositions/13136202/actions/publish
             // $cmd = 'curl -i -H "Content-Type: application/json" -X POST https://zenodo.org/api/deposit/depositions/13136202/actions/publish?access_token='.ZENODO_TOKEN;
-                     //curl -i -X POST https://zenodo.org/api/deposit/depositions/13635445/actions/publish?access_token=ACCESS_TOKEN
-
+            //         curl -i                                     -X POST https://zenodo.org/api/deposit/depositions/13635445/actions/publish?access_token=ACCESS_TOKEN
             if(!$data) {
                 $cmd = 'curl -s -H "Content-Type: application/json" -X POST '.$publish.'?access_token='.ZENODO_TOKEN; //works OK
                 // $cmd = 'curl -i -X POST '.$publish.'?access_token='.ZENODO_TOKEN; //does not work?
@@ -789,7 +788,6 @@ class ZenodoAPI extends ZenodoConnectorAPI
                 $cmd = 'curl -s -H "Content-Type: application/json" -X POST  --data '."'$json'".' '.$publish.'?access_token='.ZENODO_TOKEN; //didn't work, param wasn't submitted.
             }
             // $cmd .= " 2>&1";
-
             echo "\npublish cmd: [$cmd]\n";
             $json = shell_exec($cmd);               echo "\n$json\n";
             $obj = json_decode(trim($json), true);  echo "\n=====published=====\n"; print_r($obj); echo "\n=====published end=====\n";
