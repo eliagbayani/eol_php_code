ab5e65a367c2468b12c911576e58954e9a2fb053
eliagbayani <eagbayani@eol.org>
2024-09-16 23:15:26 +0800

    Update DataHub_GBIF_API.php

--- lib/connectors/DataHub_GBIF_API.php
+++ lib/connectors/DataHub_GBIF_API.php
@@ -47,8 +47,9 @@ class DataHub_GBIF_API
         $what = "Data_coverage";
         $func = new GBIFdownloadRequestAPI($what);
         $download_key = $func->retrieve_key_for_taxon($what);
-        // $this->remote_csv = "https://api.gbif.org/v1/occurrence/download/request/".$download_key.".zip"; //this is now a recurring download //obsolete
-        $this->remote_csv = "/extra/other_files/Data_coverage_DwCA/GBIF_service/Data_coverage_DwCA.zip"; //this was downloaded in Jenkins
+        $this->remote_csv = "https://api.gbif.org/v1/occurrence/download/request/".$download_key.".zip"; //this is now a recurring download
+
+        // $this->remote_csv = "/extra/other_files/Data_coverage_DwCA/GBIF_service/Data_coverage_DwCA.zip"; //this was downloaded in Jenkins
         // $this->remote_csv = "https://api.gbif.org/v1/occurrence/download/request/0017632-240906103802322.zip";
         
         //step 1
