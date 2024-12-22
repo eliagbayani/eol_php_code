605e8ed05a9e1645a06dd4bd71d43f5b8d5c1cb2
eliagbayani <eagbayani@eol.org>
2024-12-23 01:30:21 +0800

    Update national_checklists_2024.php

--- update_resources/connectors/national_checklists_2024.php
+++ update_resources/connectors/national_checklists_2024.php
@@ -6,7 +6,7 @@ works with: GBIF_SQL_DownloadsAPI.code-workspace
 regular operation:
     php update_resources/connectors/national_checklists_2024.php _ '{"task":"divide_into_country_files"}'
     php update_resources/connectors/national_checklists_2024.php _ '{"task":"generate_country_checklists"}'
-when caching:
+when caching the species info: ran already
     php update_resources/connectors/national_checklists_2024.php _ '{"counter":"1"}'
     php update_resources/connectors/national_checklists_2024.php _ '{"counter":"2"}'
 */
@@ -23,11 +23,13 @@ $fields = json_decode($params['json'], true);
 $counter = @$fields['counter'];
 $task = @$fields['task'];
 
-
 $what = 'Country_checklists';
 $func = new NationalChecklistsAPI($what);
-// $func->start($counter, $task); //main operation
-$func->show_countries_metadata(); //utility --- works OK
+$func->start($counter, $task); //main operation
+/*
+$func->show_countries_metadata(); //utility, generates https://editors.eol.org/other_files/GBIF_occurrence/Country_checklists/countries.tsv --- works OK | ran already
+*/
+
 /* copied template, not used here.
 Functions::finalize_dwca_resource($resource_id, false, true, $timestart); //3rd param if false it will not remove working folder
 */
