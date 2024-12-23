f939dfe894fc33735742afdd6938f91a42d89040
eliagbayani <eagbayani@eol.org>
2024-12-23 22:53:26 +0800

    Update NationalChecklistsAPI.php

--- lib/connectors/NationalChecklistsAPI.php
+++ lib/connectors/NationalChecklistsAPI.php
@@ -198,7 +198,8 @@ class NationalChecklistsAPI
                 $this->country_name = $ret['orig'];
 
                 // /* manual filter, dev only
-                if(!in_array($this->country_name, array('Trinidad and Tobago', 'Philippines', 'United States'))) continue;
+                // if(!in_array($this->country_name, array('Philippines', 'United States'))) continue;
+                if(!in_array($this->country_name, array('Australia'))) continue;
                 // */
 
                 // /*
