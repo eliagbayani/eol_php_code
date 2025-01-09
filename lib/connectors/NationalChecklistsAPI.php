b681e2ad6eef669e332d66c62f7af5712bc4b612
eliagbayani <eagbayani@eol.org>
2025-01-09 21:29:55 +0800

    Update NationalChecklistsAPI.php

--- lib/connectors/NationalChecklistsAPI.php
+++ lib/connectors/NationalChecklistsAPI.php
@@ -605,9 +605,12 @@ class NationalChecklistsAPI
         if($country == 'Timor-Leste') $country = 'East Timor';
         if($country == 'US Virgin Islands') $country = 'U.S. Virgin Islands';
         if($country == 'Wallis et Futuna') $country = 'Wallis Et Futuna Islands';
-        if($country == 'xxx') $country = 'yyy';
-        if($country == 'xxx') $country = 'yyy';
-        // */
+
+        if($country == 'Eswatini') return 'http://www.geonames.org/934841';
+        if($country == 'Bosnia and Herzegovina') return 'http://www.geonames.org/3277605';
+        if($country == 'Myanmar') return 'http://www.geonames.org/1327865';
+
+            // */
 
         if($uris = @$this->value_uris[$country]) {
             if(count($uris) == 1) return $uris[0];
