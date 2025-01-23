12d3c12c6844b7ecc1ed427c7f101949b643be30
eliagbayani <eagbayani@eol.org>
2025-01-24 01:26:05 +0800

    Update NationalChecklistsAPI.php

--- lib/connectors/NationalChecklistsAPI.php
+++ lib/connectors/NationalChecklistsAPI.php
@@ -291,7 +291,7 @@ class NationalChecklistsAPI
         $files = $this->country_path . "/*.tsv"; echo "\n[$files]\n";
         foreach(glob($files) as $file) { $i++; //echo "\n$file\n"; exit;
 
-            $this->check_species_exclusion_for_this_ctry = false; //important: initialize to false for every country
+            $this->check_species_exclusion_for_this_ctry = false; //IMPORTANT: initialize to false for every country
 
             // /* breakdown when caching
             if($counter) {
