34abc52df9dba88fc87f4e8187d04fef846ad0c2
eliagbayani <eagbayani@eol.org>
2025-02-08 16:40:01 +0800

    Update WaterBodyChecklistsAPI.php

--- lib/connectors/WaterBodyChecklistsAPI.php
+++ lib/connectors/WaterBodyChecklistsAPI.php
@@ -48,7 +48,7 @@ class WaterBodyChecklistsAPI
     }
     function generate_report($what) //'waterbodies' or 'countries'
     {
-        $report = $this->destination . $what . "_report.tsv";
+        $report = $this->destination . $what . "_report.tsv"; //countries_report.tsv OR waterbodies_report.tsv
         if($what == 'countries') $report = str_replace('WaterBody_checklists', 'Country_checklists', $report);
         if(file_exists($report)) unlink($report);
         // /*
