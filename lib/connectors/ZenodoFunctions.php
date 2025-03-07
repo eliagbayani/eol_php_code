e33f81cdf3155c9bb312d4088b20a9021deda304
eliagbayani <eagbayani@eol.org>
2025-03-07 12:41:21 +0800

    Update ZenodoFunctions.php

--- lib/connectors/ZenodoFunctions.php
+++ lib/connectors/ZenodoFunctions.php
@@ -22,29 +22,49 @@ class ZenodoFunctions
         // $obj_1st = $this->retrieve_dataset($zenodo_id); print_r($obj_1st); exit("\nstop muna 1a\n");
         $options = $this->download_options;
         $options['expire_seconds'] = 60*60*24*30; //designed to expire monthly 30 days
-        // $options['expire_seconds'] = 0;
+        $options['expire_seconds'] = 0;
         if($json = Functions::lookup_with_cache($this->api['record'].$zenodo_id, $options)) {
             $o = json_decode($json, true); //print_r($o);
-            self::save_stats($o);
+            $rek = self::retrieve($o);         print_r($rek);
+            $rek = self::append_stats_today($rek, $o);
+            ksort($rek);
+            print_r($rek);
         }
     }
-    private function save_stats($o)
+    private function retrieve($o)
     {
         $conceptrecid = $o['conceptrecid'];
-        $date = date('Y-m-d');
-        $stats[$date] = $o['stats'];
-        print_r($stats);
-
         $md5_id = md5($conceptrecid);
         if($rek = $this->cache->retrieve_json_obj($md5_id, false)) {echo "\nretrieved...\n";} //2nd param false means returned value is an array()
         else {
+            $date = date('Y-m-d');
+            $o['stats']['conceptrecid'] = $conceptrecid;
+            $stats[$date] = $o['stats']; // print_r($stats);
             $json = json_encode($stats);
             $this->cache->save_json($md5_id, $json);
             echo "\nsaved...\n";
             if($rek = $this->cache->retrieve_json_obj($md5_id, false)) {} //to check if file is really saved
             else exit("\nShould not go here [ZenodoFunctions].\n");
         }
-        print_r($rek);
+        return $rek;
+    }
+    private function append_stats_today($rek, $o)
+    {
+        $date = date('Y-m-d');
+        if(!isset($rek[$date])) {
+            // step 1
+            $conceptrecid = $o['conceptrecid'];
+            $md5_id = md5($conceptrecid);
+            $date = date('Y-m-d');
+            $o['stats']['conceptrecid'] = $conceptrecid;
+            // step 2
+            $rek[$date] = $o['stats'];
+            $json = json_encode($rek);
+            $this->cache->save_json($md5_id, $json);
+            echo "\nappend stats today...\n";
+        }
+        else echo "\nstats today already exists\n";
+        return $rek;
     }
 
 }
