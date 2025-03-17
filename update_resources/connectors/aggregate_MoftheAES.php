f604af0f4e12c26c974117957ad76e2d05449441
eliagbayani <eagbayani@eol.org>
2025-03-17 15:22:28 +0800

    Update aggregate_MoftheAES.php

--- update_resources/connectors/aggregate_MoftheAES.php
+++ update_resources/connectors/aggregate_MoftheAES.php
@@ -19,4 +19,48 @@ $resource_ids = array("118935", "120081", "120082", "118986", "118920", "120083"
 */
 $func->combine_MoftheAES_DwCAs($resource_ids);
 Functions::finalize_dwca_resource($resource_id, false, true, $timestart);
+
+/*
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/118935.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/120081.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/120082.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/118986.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/118920.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/120083.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/118237.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/118941.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/118950.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/118936.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/118946.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/118978.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/119035.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/119187.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/119188.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/119520.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/120602.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/27822.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/30354.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/30355.tar.gz
+
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/118935_ENV.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/120081_ENV.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/120082_ENV.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/118986_ENV.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/118920_ENV.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/120083_ENV.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/118237_ENV.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/118941_ENV.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/118950_ENV.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/118936_ENV.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/118946_ENV.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/118978_ENV.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/119035_ENV.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/119187_ENV.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/119188_ENV.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/119520_ENV.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/120602_ENV.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/27822_ENV.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/30354_ENV.tar.gz
+wget https://editors.eol.org/eol_php_code/applications/content_server/resources/30355_ENV.tar.gz
+*/
 ?>
