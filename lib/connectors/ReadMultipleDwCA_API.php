3859ccd9165dce2fe83c16487206423ac628dd87
eliagbayani <eagbayani@eol.org>
2025-03-19 08:28:02 +0800

    Update ReadMultipleDwCA_API.php

--- lib/connectors/ReadMultipleDwCA_API.php
+++ lib/connectors/ReadMultipleDwCA_API.php
@@ -35,7 +35,7 @@ class DwCA_Aggregator extends DwCA_Aggregator_Functions
             'expire_seconds'     => false, //expires set to false for now
             'download_wait_time' => 2000000, 'timeout' => 60*5, 'download_attempts' => 1, 'delay_in_minutes' => 1, 'cache' => 1);
     }
-    function combine_DwCAs($langs, $preferred_rowtypes = array())
+    function process_DwCAs($langs, $preferred_rowtypes = array())
     {
         foreach($langs as $this->lang) {
             echo "\n---Processing: [$this->lang]---\n";
@@ -67,24 +67,17 @@ class DwCA_Aggregator extends DwCA_Aggregator_Functions
         $index = $this->let_media_document_go_first_over_description($index); // print_r($index); exit;
         foreach($index as $row_type) {
 
+            /* copied template
             if(in_array($this->resource_id, array('71', '80', 'wikipedia_combined_languages', 'wikipedia_combined_languages_batch2'))) {} //Wikimedia commons, Wikipedia
             elseif(stripos($dwca_file, "wikipedia") !== false) {} //found string
             elseif(stripos($dwca_file, "of10") !== false) {} //found string
             elseif(stripos($dwca_file, "of6") !== false) {} //found string
             else { //as of Oct 2024 - I'm thinking I'm not sure why I excluded media objects starting Jul 8, 2024.
-                if(in_array($this->resource_id, array("NorthAmericanFlora_All_2025", "MoftheAES_resources"))) {} //does not remove media rowtype
-                else {
-                    // /* NEW: remove media rowtype: Jul 8, 2024
-                    if($row_type == strtolower("http://eol.org/schema/media/Document")) continue;
-                    // */    
-                }
-            }
-
-            /* ----------customized start------------ */
-            if($this->resource_id == 'wikipedia_combined_languages') break; //all extensions will be processed elsewhere.
-            if($this->resource_id == 'wikipedia_combined_languages_batch2') break; //all extensions will be processed elsewhere.
-            /* ----------customized end-------------- */
-
+            } */
+            // /* NEW: remove media rowtype
+            if($row_type == strtolower("http://eol.org/schema/media/Document")) continue;
+            // */    
+    
             // /* copied template -- where regular DwCA is processed.
             if($preferred_rowtypes) {
                 if(!in_array($row_type, $preferred_rowtypes)) continue;
@@ -92,9 +85,6 @@ class DwCA_Aggregator extends DwCA_Aggregator_Functions
             if($extension_row_type = @$this->extensions[$row_type]) { //process only defined row_types
                 // if($extension_row_type == 'document') continue; //debug only
                 echo "\nprocessing...: [$row_type]: ".$extension_row_type."...\n";
-                /* not used - copied template
-                self::process_fields($harvester->process_row_type($row_type), $extension_row_type);
-                */
                 self::process_table($tables[$row_type][0], $extension_row_type, $row_type);
             }
             else echo "\nun-initialized: [$row_type]: ".$extension_row_type."\n";
@@ -102,16 +92,17 @@ class DwCA_Aggregator extends DwCA_Aggregator_Functions
         }
         
         // /* ================================= start of customization =================================
+        /* copied template
         if(in_array($this->resource_id, array('wikipedia_combined_languages', 'wikipedia_combined_languages_batch2'))) {
             $tables = $info['harvester']->tables;
             // print_r($tables); exit;
-            /*Array(
-                [0] => http://rs.tdwg.org/dwc/terms/taxon
-                [1] => http://eol.org/schema/media/document
-            )*/
+            // Array(
+            //     [0] => http://rs.tdwg.org/dwc/terms/taxon
+            //     [1] => http://eol.org/schema/media/document
+            // )
             self::process_table($tables['http://rs.tdwg.org/dwc/terms/taxon'][0], 'taxon');
             self::process_table($tables['http://eol.org/schema/media/document'][0], 'document');
-        }
+        } */
         // ================================= end of customization ================================= */ 
         
         // /* un-comment in real operation -- remove temp dir
@@ -150,32 +141,19 @@ class DwCA_Aggregator extends DwCA_Aggregator_Functions
     }
     private function process_table($meta, $what, $row_type = "")
     {   //print_r($meta);
-        $meta = self::adjust_meta_value($meta, $what); //only client for now is resource "TreatmentBank" from treatment_bank.php
         // echo "\nprocessing [$what]...\n";
         $i = 0;
-        foreach(new FileIterator($meta->file_uri) as $line => $row) {
-            // $row = Functions::conv_to_utf8($row); //new line
-            
-            $i++; if(($i % 100000) == 0) echo "\n".number_format($i);
-            
-            /* new block
-            if($i == 1) {
-                $tmp = explode("\t", $row);
-                $column_count = count($tmp);
-            }
-            */
-            
+        foreach(new FileIterator($meta->file_uri) as $line => $row) {            
+            $i++; if(($i % 100000) == 0) echo "\n".number_format($i);            
             if($meta->ignore_header_lines && $i == 1) continue;
             if(!$row) continue;
             // $row = Functions::conv_to_utf8($row); //possibly to fix special chars. but from copied template
             $tmp = explode("\t", $row);
             if(!$tmp) continue;
-            
-            // if($column_count != count($tmp)) continue; //new line
-            
-            // /* New: Sep 2, 2021 -> customization needed since some fields from partner's DwCA is not recognized by EOL
-            // print_r($meta->fields); exit;
+
             $excluded_terms = array();
+            /* copied template: New: Sep 2, 2021 -> customization needed since some fields from partner's DwCA is not recognized by EOL
+            // print_r($meta->fields); exit;
             if($this->resource_id == "TreatmentBank" && $what == 'taxon') {
                 $excluded_terms = array('http://plazi.org/terms/1.0/basionymAuthors', 'http://plazi.org/terms/1.0/basionymYear', 'http://plazi.org/terms/1.0/combinationAuthors', 'http://plazi.org/terms/1.0/combinationYear', 'http://plazi.org/terms/1.0/verbatimScientificName');
             }
@@ -183,7 +161,7 @@ class DwCA_Aggregator extends DwCA_Aggregator_Functions
             if($this->resource_id == "TreatmentBank" && $what == 'document') {
                 $replaced_terms["http://rs.tdwg.org/dwc/terms/additionalInformationURL"] = "http://rs.tdwg.org/ac/terms/furtherInformationURL";
             }
-            // */
+            */
 
             $rec = array(); $k = 0;
             foreach($meta->fields as $field) {
@@ -197,10 +175,9 @@ class DwCA_Aggregator extends DwCA_Aggregator_Functions
                 $k++;
             }
             $rec = array_map('trim', $rec);
-            // print_r($rec); exit("\ndebug...\n");
+            print_r($rec); exit("\ndebug...\n");
 
             // if($what == "document") { print_r($rec); exit("\n111\n"); }
-
             /*Array(
                 [http://rs.tdwg.org/dwc/terms/taxonID] => Q140
                 [http://purl.org/dc/terms/source] => http://ta.wikipedia.org/w/index.php?title=%E0%AE%9A%E0%AE%BF%E0%AE%99%E0%AF%8D%E0%AE%95%E0%AE%AE%E0%AF%8D&oldid=2702618
@@ -209,14 +186,6 @@ class DwCA_Aggregator extends DwCA_Aggregator_Functions
                 [http://rs.tdwg.org/dwc/terms/taxonRank] => species
                 [http://rs.tdwg.org/dwc/terms/scientificNameAuthorship] => Carl Linnaeus, 1758
             )*/
-
-            /* special case. Selected by openning media.tab using Numbers while set description = 'test'. Get taxonID for that row */
-            // if($this->lang == 'el') {
-                // if($rec['http://rs.tdwg.org/dwc/terms/taxonID'] == 'Q18498') continue; 
-            // }
-            // if($this->lang == 'mk') {
-                // if(in_array($rec['http://rs.tdwg.org/dwc/terms/taxonID'], array('Q10876', 'Q5185', 'Q10892', 'Q152', 'Q10798', 'Q8314', 'Q15574019'))) continue;
-            // }
             
             $uris = array_keys($rec);
             if($what == "taxon")                    $o = new \eol_schema\Taxon();
@@ -231,10 +200,6 @@ class DwCA_Aggregator extends DwCA_Aggregator_Functions
             if($what == "taxon") {
                 //----------taxonID must be unique
                 $taxon_id = $rec['http://rs.tdwg.org/dwc/terms/taxonID'];
-
-                // if($taxon_id == "EC1F5479EE323E211667122BCBA032DA.taxon") { //debug only
-                //     print_r($rec); exit("\nstop 1\n");
-                // }
                 
                 //----------sciname must not be blank
                 if(!$rec['http://rs.tdwg.org/dwc/terms/scientificName']) continue;
@@ -301,25 +266,7 @@ class DwCA_Aggregator extends DwCA_Aggregator_Functions
             }
             */
             
-            //================== start attributions =================== https://eol-jira.bibalex.org/browse/DATA-1887?focusedCommentId=66290&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-66290
-            /* Attributions:    You can use the two output columns for bibliographicCitation and FurtherInformationURL in the media file, 
-                                and for bibliographicCitation and source in the MoF file. */
-            if($this->attributions) { //for MoftheAES, NorthAmerican Flora, etc.
-                $citation = $this->attributions[$this->resource_id_current]['citation'];
-                $source = $this->attributions[$this->resource_id_current]['source'];
-                if($what == "document") {
-                    $rec['http://purl.org/dc/terms/bibliographicCitation'] = $citation;
-                    $rec['http://rs.tdwg.org/ac/terms/furtherInformationURL'] = $source;
-                }
-                elseif(in_array($what, array("measurementorfact", "association"))) {
-                    $rec['http://purl.org/dc/terms/bibliographicCitation'] = $citation;
-                    $rec['http://purl.org/dc/terms/source'] = $source;
-                }
-                $uris = array_keys($rec);
-            }
-            //================== end attributions ===================
-            
-            
+                        
             $uris = array_keys($rec); // print_r($uris);
             foreach($uris as $uri) {
                 $field = pathinfo($uri, PATHINFO_BASENAME);
@@ -339,34 +286,10 @@ class DwCA_Aggregator extends DwCA_Aggregator_Functions
                 $o->$field = $rec[$uri];
             }
             
-            // /* ----- new: add a new text object using <title> tag from eml.xml. Will practically double the no. of text objects. Per https://eol-jira.bibalex.org/browse/DATA-1896?focusedCommentId=66921&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-66921
-            // Dec 4, 2023: Moved up. This will no longer double the no. of text objects but will overwrite the original text object we use to textmine.
-            if($this->resource_id == "TreatmentBank") {
-                if($what == "document") {
-                    if($row_type == 'http://eol.org/schema/media/document') { //not for http://rs.gbif.org/terms/1.0/description
-                        if($title = self::get_title_from_eml_xml()) {
-                            $o->identifier = md5($title.$o->taxonID);
-                            $o->title = 'Title for eol-geonames';
-                            $o->description = $title;
-                            $o->bibliographicCitation = '';
-                            if(!isset($this->data_objects[$o->identifier])) {
-                                $this->archive_builder->write_object_to_file($o);
-                                $this->data_objects[$o->identifier] = '';
-                            }
-                            continue;
-                        }    
-                    }                
-                }
-            }
-            // ----- */
-
             $this->archive_builder->write_object_to_file($o);
 
             // if($i >= 2) break; //debug only
         } //end foreach()
-    }
-    private function get_title_from_eml_xml()
-    {
     }
 }
 ?>
