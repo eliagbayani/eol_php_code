08871ffed347b29a758342a961e324b9240cf8c9
eliagbayani <eagbayani@eol.org>
2025-03-20 10:49:44 +0800

    Update GBIFMapDataAPI.php

--- lib/connectors/GBIFMapDataAPI.php
+++ lib/connectors/GBIFMapDataAPI.php
@@ -233,20 +233,22 @@ class GBIFMapDataAPI
             // if(in_array(strtolower($first_char), array('a'))) {} else continue;              //1
             // if(in_array(strtolower($first_char), array('e','f'))) {} else continue;          //2
             // if(in_array(strtolower($first_char), array('i','j','k'))) {$this->auto_refresh_mapYN = true;} else continue;      //3
-            // if(in_array(strtolower($first_char), array('m','n','o'))) {} else continue;      //4
+            // if(in_array(strtolower($first_char), array('m','n'))) {} else continue;      //4
             // if(in_array(strtolower($first_char), array('q','r','t'))) {} else continue;      //5
             // if(in_array(strtolower($first_char), array('u','v','w'))) {$this->auto_refresh_mapYN = true;} else continue;      //6
             
             // if(in_array(strtolower($first_char), array('x','y','z'))) {$this->auto_refresh_mapYN = true;} else continue;      //7 DONE
             // if(in_array(strtolower($first_char), array('s'))) {} else continue;      //7
 
-            // if(in_array(strtolower($first_char), array('g'))) {} else continue;              //8
+            if(in_array(strtolower($first_char), array('g'))) {$this->auto_refresh_mapYN = true;} else continue;              //8
             // if(in_array(strtolower($first_char), array('d'))) {} else continue;              //9
             // if(in_array(strtolower($first_char), array('l'))) {} else continue;              //10
             // if(in_array(strtolower($first_char), array('c'))) {} else continue;              //11
             // if(in_array(strtolower($first_char), array('h'))) {} else continue;              //12
             // if(in_array(strtolower($first_char), array('b'))) {} else continue;              //13
-            if(in_array(strtolower($first_char), array('p'))) {} else continue;              //14
+            // if(in_array(strtolower($first_char), array('p'))) {} else continue;              //14
+            // if(in_array(strtolower($first_char), array('o'))) {} else continue;              //15
+
             // */
 
             if($rec['taxonRank'] == 'species') {} //run only species-level taxa at this point
