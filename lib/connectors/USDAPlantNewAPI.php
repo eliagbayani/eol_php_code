14f66b7bbebc2198e41fecb2ecbe5a0c5aebf6bd
eliagbayani <eagbayani@eol.org>
2025-06-02 12:49:02 +0800

    Update USDAPlantNewAPI.php

--- lib/connectors/USDAPlantNewAPI.php
+++ lib/connectors/USDAPlantNewAPI.php
@@ -75,7 +75,7 @@ class USDAPlantNewAPI
         /* copied template
         $this->temp_path = CONTENT_RESOURCE_LOCAL_PATH . "BOLDS_temp/";
         if(Functions::is_production()) $this->BOLDS_new_path = "https://editors.eol.org/eol_connector_data_files/BOLDS_new/";
-        else                           $this->BOLDS_new_path = "http://localhost/cp/BOLDS_new/";
+        else                           $this->BOLDS_new_path = LOCAL_HOST."/cp/BOLDS_new/";
         */
     }
     function initialize()
@@ -189,7 +189,10 @@ class USDAPlantNewAPI
                 // print_r($profile); exit("\nhas no images!\n");
             }
         }
-        else exit("\ncannot lookup\n");
+        else {
+            echo "\ncannot lookup: ".$rec['Symbol']."\n";
+            $this->debug['cannot lookup'][$rec['Symbol']] = '';
+        }
     }
     private function get_images($obj)
     {
